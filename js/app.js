acb22fb51b39ed680529acbdf066bdc06a092042
aidenkim9 <an010719@naver.com>
2024-04-07 18:18:19 +0900

    login-form css updated

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const loginInput = document.querySelector("#login-form input");
 const loginBtn = document.querySelector("#login-form button");
 const loginForm = document.querySelector("#login-form");
 const greeting = document.querySelector("#greeting");
+const afterLogIn = document.querySelector("#after-login");
 const HIDDEN_CLASSNAME = "hidden";
 
 function onSubmitBtn(event) {
@@ -15,12 +16,14 @@ function onSubmitBtn(event) {
 function handleGreeting(username) {
   greeting.innerText = `hello ${username}!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  afterLogIn.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const USERNAME_KEY = localStorage.getItem("username");
 
 if (USERNAME_KEY === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
+  afterLogIn.classList.add(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onSubmitBtn);
 } else {
   handleGreeting(USERNAME_KEY);
